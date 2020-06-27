8c594f01128dd39ba4b17253e204fe24cf67ff41
jecaro <jeancharles.quillet@gmail.com>
2020-06-27 15:09:55 +0200

    Remove unneeded files

--- bin/efi.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh -x
-
-efibootmgr -d /dev/sda -p 1 -c -L "Arch Linux" -l /vmlinuz-linux -u "quiet root=/dev/mapper/vglaptruc-lvroot rw resume=/dev/mapper/vglaptruc-lvroot resume_offset=329728 initrd=/initramfs-linux.img"
