2c06c519096b28dddc0ab34daa4ff4902fa9181d
jecaro <jeancharles.quillet@gmail.com>
2021-10-06 11:01:11 +0200

    add compose key to keyboard options

--- bin/bin/setup-keyboard.sh
+++ bin/bin/setup-keyboard.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-setxkbmap -layout fr,fr -variant nodeadkeys, -option caps:escape,grp:ctrls_toggle
+setxkbmap -layout fr,fr -variant nodeadkeys, -option caps:escape,grp:ctrls_toggle,compose:menu
