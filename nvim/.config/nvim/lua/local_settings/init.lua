3418832e49e5b68d40818e488081c99f85875b88
jecaro <jeancharles.quillet@gmail.com>
2022-05-28 21:34:21 +0200

    handle xmonad repository

--- nvim/.config/nvim/lua/local_settings/init.lua
+++ nvim/.config/nvim/lua/local_settings/init.lua
@@ -1,20 +1,34 @@
-
 local M = {}
 
+function without_ext(filename)
+    return filename:gsub('%.%w+$', '')
+end
+
 function basename(filename)
-    local without_ext = filename:gsub('%.%w+$', '')
-    return string.match(without_ext, '[%w-]+$')
+    return filename:match('[^/]+$')
+end
+
+-- lua is unable to load a .module.lua file so the .xmonad repo is handled by
+-- the file xmonad.lua
+function unhide(filename)
+    return filename:gsub('^%.', '')
 end
 
 repos = {}
+M.repos = {}
 
 for filename in io.popen('ls ~/.config/nvim/lua/local_settings/repositories/*.lua'):lines() do
-    local name = basename(filename)
+    local name = without_ext(basename(filename))
     repos[name] = require("local_settings.repositories."..name)
+    M.repos[name] = require("local_settings.repositories."..name)
+end
+
+function modulename()
+    return unhide(basename(vim.fn.getcwd()))
 end
 
 function M.apply()
-    local name = basename(vim.fn.getcwd())
+    local name = modulename()
 
     if repos[name] and repos[name].apply then
         repos[name].apply()
@@ -22,7 +36,7 @@ function M.apply()
 end
 
 function M.format()
-    local name = basename(vim.fn.getcwd())
+    local name = modulename()
 
     if repos[name] and repos[name].format then
         repos[name].format()
@@ -30,7 +44,7 @@ function M.format()
 end
 
 function M.on_new_config(lsp)
-    local name = basename(vim.fn.getcwd())
+    local name = modulename()
 
     if repos[name] and repos[name].on_new_config then
         return (function (new_config)
